c5f716dfb4f0da6128e4891e0880ae46d29e681c
Umair Ashraf <85620958+umairashraf2@users.noreply.github.com>
2022-11-21 12:23:13 +0500

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'd^fx9%6(@_ea=(lhcg37^t7p020!k&-^atwmi^jq$k%orb9ka)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['manage-employe.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','manage-employe.herokuapp.com']
 
 
 # Application definition
