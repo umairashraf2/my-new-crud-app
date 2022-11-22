c19905b8fd04acce7f39606153d28ceefdb4ff97
Umair Ashraf <umair.dco@gmail.com>
2022-11-22 20:34:45 +0500

    fixed

--- app/urls.py
+++ app/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import Home,Login,Signup,Signout,Read,delete,Search
+from app.views import Home, Login, Signup, Signout, Read, delete, Search
 
 urlpatterns = [
-    path('', Home, name='home'),
-    path('employee_update/<int:id>/', Home, name='employee_update'),
-    path('login/' , Login  , name='login'),
-    path('signup/' , Signup ),
-    path('logout/' , Signout ), 
-    path('read/' , Read, name='read'),
-    path('delete/<int:id>/', delete, name='delete'),
-    path('search/' , Search, name='delete'),
-    
+    path("", Home, name="home"),
+    path("employee_update/<int:id>/", Home, name="employee_update"),
+    path("login/", Login, name="login"),
+    path("signup/", Signup),
+    path("logout/", Signout),
+    path("read/", Read, name="read"),
+    path("delete/<int:id>/", delete, name="delete"),
+    path("search/", Search, name="delete"),
 ]
