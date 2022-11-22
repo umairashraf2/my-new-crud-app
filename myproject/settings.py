9f2e2b183ef4e29d7c3dca7235c724fea4662a78
Umair Ashraf <umair.dco@gmail.com>
2022-11-22 20:32:08 +0500

    fixed

--- myproject/settings.py
+++ myproject/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "staticfiles"
+# ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
